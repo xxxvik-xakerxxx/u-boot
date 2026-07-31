4977e1c7d497d68d9265aa62c7df21ea4792225e
xxxvik-xakerxxx <19531907+xxxvik-xakerxxx@users.noreply.github.com>
2026-07-31 17:18:26 +0300

    board: mediatek: tetris: register modem connsys EMI

--- board/mediatek/mt6878/mt6878_tetris.c
+++ board/mediatek/mt6878/mt6878_tetris.c
@@ -20,6 +20,7 @@ DECLARE_GLOBAL_DATA_PTR;
 
 #define MTK_SIP_CONNSYS_EMI_SET		0xc200041a
 #define MTK_SIP_CONNSYS_EMI_MAIN	0
+#define MTK_SIP_CONNSYS_EMI_MD		1
 #define MTK_SIP_CONNSYS_EMI_GPS		2
 #define MTK_SIP_CONNSYS_EMI_SUCCESS	0x100
 
@@ -27,6 +28,9 @@ DECLARE_GLOBAL_DATA_PTR;
 #define TETRIS_CONNSYS_EMI_SIZE		0x00c00000ULL
 #define TETRIS_GPS_EMI_BASE		0x86a00000ULL
 #define TETRIS_GPS_EMI_SIZE		0x00100000ULL
+#define TETRIS_MD_CACHE_EMI_BASE	0x88000000ULL
+#define TETRIS_MD_CACHE_EMI_SIZE	0x02560000ULL
+#define TETRIS_MD_CONNSYS_EMI_SIZE	0x00d80000ULL
 
 static int tetris_set_connsys_emi(unsigned long selector, u64 base, u64 size,
 				  const char *name)
@@ -48,11 +52,11 @@ static int tetris_set_connsys_emi(unsigned long selector, u64 base, u64 size,
 
 static int tetris_prepare_connsys_emi(const void *fdt)
 {
-	fdt_size_t reserved_size;
-	fdt_addr_t base;
+	fdt_size_t md_reserved_size, reserved_size;
+	fdt_addr_t base, md_base;
 	const fdt32_t *memory_region;
 	u64 emi_size, gps_base, gps_size;
-	int consys, gps, len, reserved, ret;
+	int consys, gps, len, md_reserved, reserved, ret;
 
 	consys = fdt_node_offset_by_compatible(fdt, -1,
 					       "mediatek,mt6878-6631-consys");
@@ -106,8 +110,32 @@ static int tetris_prepare_connsys_emi(const void *fdt)
 		return -FDT_ERR_BADVALUE;
 	}
 
+	md_reserved = fdt_node_offset_by_compatible(fdt, -1,
+						    "mediatek,ap_md_c_smem");
+	if (md_reserved < 0) {
+		printf("Tetris: AP/MD cache shared memory missing: %s\n",
+		       fdt_strerror(md_reserved));
+		return md_reserved;
+	}
+
+	md_base = fdtdec_get_addr_size_auto_noparent(fdt, md_reserved, "reg", 0,
+						     &md_reserved_size, true);
+	if (md_base != TETRIS_MD_CACHE_EMI_BASE ||
+	    md_reserved_size != TETRIS_MD_CACHE_EMI_SIZE) {
+		printf("Tetris: unsafe AP/MD cache layout %llx/%llx\n",
+		       (unsigned long long)md_base,
+		       (unsigned long long)md_reserved_size);
+		return -FDT_ERR_BADVALUE;
+	}
+
 	ret = tetris_set_connsys_emi(MTK_SIP_CONNSYS_EMI_MAIN, base,
 				     emi_size, "conninfra");
+	if (ret)
+		return ret;
+
+	ret = tetris_set_connsys_emi(MTK_SIP_CONNSYS_EMI_MD, md_base,
+				     TETRIS_MD_CONNSYS_EMI_SIZE,
+				     "modem conninfra");
 	if (ret)
 		return ret;
 
